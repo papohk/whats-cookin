7b2d353f9884eae29c9e3d3eaf0fcc2525ef06a8
papohk <polly.a.po@gmnail.com>
2022-02-22 18:29:54 -0500

    experimented with features

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    try {
+        var elems = document.querySelectorAll('.carousel');
+        var instances = M.Carousel.init(elems, {});
+
+        elems = document.querySelectorAll('.slider');
+        instances = M.Slider.init(elems, {});
+
+        elems = document.querySelectorAll('.parallax');
+        instances = M.Parallax.init(elems, {});
+    } catch (error) {
+        console.log(error);
+    }
+});
