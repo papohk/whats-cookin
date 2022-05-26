33a3ff118e55d25d50c789b13ea6b04132dc94a6
papohk <polly.a.po@gmnail.com>
2022-05-26 11:20:11 -0400

    setup OAuth on Google Console

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
 const {Schema, model} = mongoose
+const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const schema = new Schema({
     username: {
