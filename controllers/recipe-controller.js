7f8fe3ae7460cd2b8058b353edecbe39223df43d
papohk <polly.a.po@gmnail.com>
2022-05-26 12:30:09 -0400

    added view, routes for recipe-details

--- controllers/recipe-controller.js
+++ controllers/recipe-controller.js
@@ -5,6 +5,19 @@ module.exports= {
         
         const response=await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${req.query.ingredients}&apiKey=${process.env.SPOONACULAR_KEY}`)
         const body=await response.json()
+        
         res.render("pages/recipe-search", { recipes: body})
+    },
+    details: async (req, res) => {
+        console.log('recipes details', req.params)
+        try {
+
+            const response=await fetch(`https://api.spoonacular.com/recipes/${req.params.id}/information?apiKey=${process.env.SPOONACULAR_KEY}`)
+            const body=await response.json()
+            console.log('details data', body)
+            res.render("pages/recipe-details", {recipe: body})
+        } catch(error) {
+            console.log('details error', error)
+        }
     }
 }
