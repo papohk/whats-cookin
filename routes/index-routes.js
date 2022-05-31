9314f18b70d09043c9bfd9246a3d4acd45fb0b3e
papohk <polly.a.po@gmnail.com>
2022-05-31 11:33:35 -0400

    updated index-route

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -9,4 +9,7 @@ router.use("/recipes", recipeRoutes)
 const userRoutes=require("./user-routes")
 router.use("/user", userRoutes)
 
+const favRoutes=require("./favlist-routes")
+router.use("/fav", favRoutes)
+
 module.exports=router
